85d06745a747b2a42a4158d6d966bbc58de893c8
Merijn <mfg.gommeren@student.avans.nl>
2022-09-18 16:12:17 +0200

    .

--- SharedGameLogic/GameData/Game.cs
+++ SharedGameLogic/GameData/Game.cs
@@ -5,7 +5,14 @@ namespace SharedGameLogic.GameData;
 
 public class Game
 {
+    public Thread GameThread { get; set; }
     public Game(List<TcpClient> clients)
+    {
+        GameThread = new Thread(Run);
+        GameThread.Start();
+    }
+
+    public void Run()
     {
         
     }
