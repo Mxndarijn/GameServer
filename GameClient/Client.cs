8068cf2a3e401cfe67dd6d36712651d813aab0c5
Merijn <mfg.gommeren@student.avans.nl>
2022-09-18 15:36:33 +0200

    .

---
+++ GameClient/Client.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    private TcpClient _client;
+
+    public Client()
+    {
+        try
+        {
+            _client = new("86.95.122.200", 2460);
+            WriteMessageToServer("RequestConnection");
+        }
+        catch
+        {
+            Console.WriteLine("Could not connect with server...");
+        }
+
+    }
+
+    public void WriteMessageToServer(string message)
+    {
+        var stream = new StreamWriter(_client.GetStream(), Encoding.ASCII);
+        {
+            stream.WriteLine(message);
+            stream.Flush();
+        }
+    }
+
+    public string ReadResponseMessage()
+    {
+        var stream = new StreamReader(_client.GetStream(), Encoding.ASCII);
+        {
+            return stream.ReadLine();
+        }
+    }
+}
---
+++ GameClient/Program.cs
@@ -0,0 +1,10 @@
+namespace GameClient;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Client client = new Client();
+        Console.WriteLine("Client Created");
+    }
+}
